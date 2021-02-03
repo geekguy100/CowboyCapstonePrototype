ae50bda4bedf58ec4abea4198a235d1ae8ac52e6
Kyle Grenier <kylegrenier@users.noreply.github.com>
2021-02-03 14:23:53 -0600

    Implemented player stamina regen

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -5,7 +5,11 @@ public class PlayerInputController : MonoBehaviour
 {
     [SerializeField] private float maxStamina = 10f;
                      private float currentStamina;
-    [SerializeField] private float staminaDecreaseMultiplier = 5f;
+    [SerializeField] private float staminaDecreaseMultiplier = 1f;
+    [SerializeField] private float staminaIncreaseMultiplier = 1f;
+
+    [SerializeField] private float rechargeWaitTime = 2f;
+                     private float currentRechargeTime = 0f;
 
     private CharacterMovement characterMovement;
 
@@ -26,13 +30,28 @@ public class PlayerInputController : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(h, v);
 
-        //If the player is moving, decrease stamina.
-        if (currentStamina > 0 &&  dir != Vector3.zero)
-            DecreaseStamina();
-
         //If the player has the stamina to move, do so.
         if (currentStamina > 0)
             characterMovement.Move(dir);
+
+        //If the player is moving, decrease stamina.
+        //If they are standing still, increase stamina.
+        if (currentStamina > 0 && dir != Vector3.zero)
+        {
+            //Reset recharge timer.
+            if (currentRechargeTime > 0)
+                currentRechargeTime = 0;
+
+            DecreaseStamina();
+        }
+        else if (currentStamina < maxStamina && dir == Vector3.zero) //Player can recharge stamina since they're standing still.
+        {
+            //If the recharge time is not maxed, keep waiting.
+            if (currentRechargeTime < rechargeWaitTime)
+                currentRechargeTime += Time.deltaTime;
+            else //Once the recharge time is maxed, start increasing stamina.
+                IncreaseStamina();
+        }
     }
 
     /// <summary>
@@ -40,8 +59,17 @@ public class PlayerInputController : MonoBehaviour
     /// </summary>
     private void DecreaseStamina()
     {
-        print("Stamina = " + currentStamina);
+        //print("Stamina = " + currentStamina);
         currentStamina -= Time.deltaTime * staminaDecreaseMultiplier;
+        OnStaminaChange?.Invoke(currentStamina);
+    }
+
+    /// <summary>
+    /// Increases the stamina over time and with the multiplier in mind.
+    /// </summary>
+    private void IncreaseStamina()
+    {
+        currentStamina += Time.deltaTime * staminaIncreaseMultiplier;
         OnStaminaChange?.Invoke(currentStamina);
     }
 }
